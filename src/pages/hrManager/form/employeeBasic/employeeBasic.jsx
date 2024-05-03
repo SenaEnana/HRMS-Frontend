23ab4662a40d8741aecae9076646104750e4b4d9
NazrawitGemechu <nazrawitgemechu9706@gmail.com>
2024-05-03 21:53:59 +0300

    added roles

--- src/pages/hrManager/form/employeeBasic/employeeBasic.jsx
+++ src/pages/hrManager/form/employeeBasic/employeeBasic.jsx
@@ -151,6 +151,7 @@ function EmployeeBasic() {
             FirstName: "",
             LastName: "",
             Email: "",
+            Roles: "",
             Gender: "",
             MotherName: "",
             Region: "",
