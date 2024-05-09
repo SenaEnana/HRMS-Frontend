7340a1e9e40fa0f880f01fa3892d1055f1da8b8f
NazrawitGemechu <nazrawitgemechu9706@gmail.com>
2024-05-09 09:53:59 +0300

    Connedted comlaint and my leave requests

--- src/pages/employee/dashboard/employeeDashboard.jsx
+++ src/pages/employee/dashboard/employeeDashboard.jsx
@@ -13,12 +13,22 @@ import { useNavigate } from "react-router";
 
 const EmployeeDashboard = () => {
   const [leaveBalances, setLeaveBalances] = useState([]);
+  const [leaveRequests, setLeaveRequests] = useState([]);
+  const [compliantRequests, setCompliantRequests] = useState([]);
   const navigate = useNavigate();
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
   useEffect(() => {
     fetchLeaveBalance();
+    fetchLeaveRequests();
+    fetchMyCompliant();
   }, []);
 
   async function fetchLeaveBalance() {
@@ -54,6 +64,72 @@ const EmployeeDashboard = () => {
       console.error("Error fetching leave balance:", error.message);
     }
   }
+  async function fetchMyCompliant() {
+    try {
+      const token = sessionStorage.getItem('token');
+      if (!token) {
+        console.error('Token not found in session storage');
+        navigate('/login');
+        return;
+      }
+
+      const isValid = isTokenValid(token);
+      if (!isValid) {
+        console.error('Invalid token');
+        navigate('/login');
+        return;
+      }
+
+      const userId = getUserIdFromToken(token);
+      console.log(userId);
+
+      const url = `https://localhost:7140/api/Complaint/MyCompliants?userId=${userId}`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch complaint requests');
+      }
+
+      const data = await response.json();
+      console.log(data)
+      setCompliantRequests(data);
+    } catch (error) {
+      console.error("Error fetching compliant requests:", error.message);
+    }
+  }
+  async function fetchLeaveRequests() {
+    try {
+      const token = sessionStorage.getItem('token');
+      if (!token) {
+        console.error('Token not found in session storage');
+        navigate('/login');
+        return;
+      }
+
+      const isValid = isTokenValid(token);
+      if (!isValid) {
+        console.error('Invalid token');
+        navigate('/login');
+        return;
+      }
+
+      const userId = getUserIdFromToken(token);
+      console.log(userId);
+
+      const url = `https://localhost:7140/api/Leave/MyLeaveRequests?userId=${userId}`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch leave requests');
+      }
+
+      const data = await response.json();
+      console.log(data)
+      setLeaveRequests(data);
+    } catch (error) {
+      console.error("Error fetching leave requests:", error.message);
+    }
+  }
 
   function isTokenValid(token) {
     if (!token) {
@@ -74,7 +150,7 @@ const EmployeeDashboard = () => {
 
   function getUserIdFromToken(token) {
     const decodedToken = JSON.parse(atob(token.split('.')[1]));
-    return decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier']; // Assuming 'nameidentifier' is a claim in your token
+    return decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'];
   }
 
   return (
@@ -107,7 +183,7 @@ const EmployeeDashboard = () => {
             <StatBox
               title={<>
                 {balance.leaveTypeName}<br />
-                 Allowed Days: {balance.allowedDays}
+                Allowed Days: {balance.allowedDays}
               </>}
 
               subtitle={`${balance.remainingLeaveBalance} days remaining`} // Using remainingLeaveBalance
@@ -146,12 +222,12 @@ const EmployeeDashboard = () => {
             p="15px"
           >
             <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
-              Recent Transactions
+              Your Leave Requests
             </Typography>
           </Box>
-          {mockTransactions.map((transaction, i) => (
+          {leaveRequests.map((leaveRequest, i) => (
             <Box
-              key={`${transaction.txId}-${i}`}
+              key={`${leaveRequest.id}-${i}`}
               display="flex"
               justifyContent="space-between"
               alignItems="center"
@@ -159,28 +235,64 @@ const EmployeeDashboard = () => {
               p="15px"
             >
               <Box>
-                <Typography
-                  className="text-dark fw-bold"
-                  variant="h5"
-                  fontWeight="600"
-                >
-                  {transaction.txId}
+                <Typography className="text-dark fw-bold" variant="h5" fontWeight="600">
+                  {leaveRequest.type}
                 </Typography>
                 <Typography color={colors.grey[100]}>
-                  {transaction.user}
+                Leave Type:{leaveRequest.leaveTypeName}
+                </Typography>
+                <Typography color={colors.grey[100]}>
+                From: {formatDate(leaveRequest.startDate)} - {formatDate(leaveRequest.endDate)}
                 </Typography>
               </Box>
-              <Box color={colors.grey[100]}>{transaction.date}</Box>
-              <Box
-                className="text-dark fs-5 fw-bold"
-                p="5px 10px"
-                borderRadius="4px"
-              >
-                ${transaction.cost}
-              </Box>
+              <Box color={colors.grey[100]}>{leaveRequest.status}</Box>
             </Box>
           ))}
         </Box>
+
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+          p="30px"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            colors={colors.grey[100]}
+            p="15px"
+          >
+            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+              Your Complaints
+            </Typography>
+          </Box>
+          {/* Replace mockTransactions with your complaint data */}
+          {compliantRequests.map((complaint, i) => (
+            <Box
+              key={`${complaint.id}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography className="text-dark fw-bold" variant="h5" fontWeight="600">
+                  Incident :{complaint.incident}
+                </Typography>
+                <Typography color={colors.grey[100]}>
+                  Date of Event: {formatDate(complaint.dateOfEvent)}
+                </Typography>
+              </Box>
+              <Box color={colors.grey[100]}>{complaint.status}</Box>
+            </Box>
+          ))}
+        </Box>
+
 
         {/* ROW 3 */}
         <Box
